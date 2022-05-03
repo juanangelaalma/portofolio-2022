71ea360dafceb4bd82fbe177164aaed95901930e
juanangelaalma <juanlah77@gmail.com>
2022-05-04 00:24:38 +0700

    add header

---
+++ src/containers/index.js
@@ -0,0 +1,7 @@
+import Header from "./header/Header";
+import Contact from "./contact/Contact";
+import Footer from "./footer/Footer";
+import Blog from "./blog/Blog"
+import Portofolio from "./portofolio/Portofolio"
+
+export { Header, Contact, Footer, Blog, Portofolio }
