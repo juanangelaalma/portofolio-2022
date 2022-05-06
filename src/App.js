484f380c49f4c75e9c8883466b69255e7fe2a79c
juanangelaalma <juanlah77@gmail.com>
2022-05-06 18:30:35 +0700

    add footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from './components/navbar/Navbar'
 
 import "./App.css"
-import { Contact, Header, Portofolio } from './containers'
+import { Contact, Footer, Header, Portofolio } from './containers'
 
 const App = () => {
   return (
@@ -13,6 +13,7 @@ const App = () => {
         <Portofolio />
         <Contact />
       </main>
+      <Footer />
     </div>
   )
 }
